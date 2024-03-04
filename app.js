943fa5cf3deeb3f5f601c2cc9b7497e35bd64d9c
mubbashiribad <159440561+mubbashiribad@users.noreply.github.com>
2024-03-04 06:46:31 -0800

    4. Famous Quote 2

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 var qoute = "A person who never made a mistake never tried anything new";
-var author = "Albert Einstein";
-console.log("".concat(author, " once said,\"").concat(qoute));
+var famous_person = "Albert Einstein";
+var message = "".concat(famous_person, " once said, ").concat(qoute);
+console.log(message);
