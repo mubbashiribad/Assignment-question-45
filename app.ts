1614ab502ff119fcfd012b8e69fb4e74cd0496f1
mubbashiribad <159440561+mubbashiribad@users.noreply.github.com>
2024-03-04 06:49:45 -0800

    7&8. Number Eight

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-let personName: string=`\n\t IBAD SHAH\t\n`;
-console.log(personName);
-let stripped: string = personName.trim();
-console.log(stripped);
+console.log(5+3);
+console.log(11 -3);
+console.log(4 *2);
+console.log(16 /2);
