df6a8c7c9778130776d5cec82ba2f0cd5a2540d6
mubbashiribad <159440561+mubbashiribad@users.noreply.github.com>
2024-03-04 06:52:47 -0800

    10. Adding Comments

---
+++ program1.js
@@ -0,0 +1,5 @@
+//exercise 10
+//my name is MubbashirulIBAD
+//Dated: 22/02/2024
+// this program will run simple code just like hello world
+console.log("hello world simple program");
---
+++ program2.ts
@@ -0,0 +1,5 @@
+//exercise 10
+//my name is MubbashirulIBAD
+//Dated: 22/02/2024
+//this program multiply and substract
+console.log(5 *2 -9);
